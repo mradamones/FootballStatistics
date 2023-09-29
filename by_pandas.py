9ae0d955420bea1ecad3d81e81d4ee947fdbbe33
mradamones <adam.szatkowski@capgemini.com>
2023-09-29 12:07:57 +0200

    Added some similariry metrics, tried to add gui

--- by_pandas.py
+++ by_pandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from utils import get_data as gd
+import math
 
 
 def normalize(tab):
@@ -9,7 +10,7 @@ def normalize(tab):
     return numeric
 
 
-def find_similar(tab, idx):
+def similar_manhattan(tab, idx):
     sample = tab.loc[idx]
     print(sample)
     winner = 0
@@ -25,7 +26,43 @@ def find_similar(tab, idx):
     return winner, min_diff
 
 
-def find_similar_avg(tab, idx):
+def similar_euclidean(tab, idx):
+    sample = tab.loc[idx]
+    print(sample)
+    winner = 0
+    min_diff = 1
+    normalized = normalize(tab)
+    for i in range(len(tab)):
+        if not tab.iloc[i].equals(tab.iloc[idx]):
+            diff = pow((normalized.loc[idx] - normalized.loc[i]), 2)
+            avg_diff = diff.mean()
+            if avg_diff < min_diff:
+                min_diff = avg_diff
+                winner = tab.iloc[i]
+    return winner, min_diff
+
+
+def similar_pearson(tab, idx):
+    sample = tab.loc[idx]
+    print(sample)
+    winner = 0
+    max_pearson = 0
+    normalized = normalize(tab)
+    for i in range(len(tab)):
+        if i != idx:
+            # pearson = pow((normalized.loc[idx] - normalized.loc[i]), 2)
+            avg_sample = normalized.loc[idx].mean()
+            avg_current = normalized.loc[i].mean()
+            pearson = ((normalized.loc[idx] - avg_sample) * (normalized.loc[i] - avg_current)).sum() / (
+                        len(normalized.columns) - 1)
+            abs_pearson = abs(pearson)
+            if abs_pearson > max_pearson:
+                max_pearson = abs_pearson
+                winner = tab.iloc[i]
+    return winner, max_pearson
+
+# TODO - create cosinus similarity
+def similar_avg(tab, idx):
     sample = tab.loc[idx]
     print(sample)
     winner = 0
@@ -42,11 +79,19 @@ def find_similar_avg(tab, idx):
 
 mids = gd.get_mids()
 
-bellingham1, min_diff_1 = find_similar(mids, 65)
+bellingham1, min_diff_1 = similar_manhattan(mids, 68)
 print(bellingham1)
-print("Lowest difference: " + str(100 * min_diff_1) + "%")
+print("Lowest manhattan difference: " + str(100 * min_diff_1) + "%")
 
-bellingham2, min_diff_2 = find_similar_avg(mids, 65)
+bellingham2, min_diff_2 = similar_avg(mids, 68)
 print(bellingham2)
-print("Lowest difference: " + str(100 * min_diff_2) + "%")
+print("Lowest average difference: " + str(100 * min_diff_2) + "%")
+
+bellingham3, min_diff_3 = similar_euclidean(mids, 68)
+print(bellingham3)
+print("Lowest euclidean difference: " + str(100 * min_diff_3) + "%")
+
+bellingham4, min_diff_4 = similar_pearson(mids, 68)
+print(bellingham4)
+print("Highest pearson correlance: " + str(100 * min_diff_4) + "%")
 # TODO - stworzyć kilka jakościowych statystyk, np (CrdY+3*CrdR)/Min
